712d435968bcc072cecb24e6822f07233588f17c
anomenaf <nomenafitia.andry@ynov.com>
2025-10-08 15:38:26 +0200

    accueil

--- server.go
+++ server.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"log"
+	"net/http"
+	"text/template"
+)
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	template, err := template.ParseFiles("./index.html", "./template/header.html", "./template/footer.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	template.Execute(w, nil)
+}
+
+func main() {
+	http.HandleFunc("/", Home)
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.ListenAndServe(":8080", nil)
+}
---
+++ src/game.go
@@ -0,0 +1,6 @@
+package power4
+
+type Players struct {
+	Player1 string
+	Player2 string
+}
