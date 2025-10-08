076270ccc80f695ba062fdb425ed753a9338a0f4
anomenaf <nomenafitia.andry@ynov.com>
2025-10-08 16:12:46 +0200

    site

--- server.go
+++ server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	power4 "power4/src"
 	"text/template"
 )
 
@@ -14,8 +15,33 @@ func Home(w http.ResponseWriter, r *http.Request) {
 	template.Execute(w, nil)
 }
 
+func Player(w http.ResponseWriter, r *http.Request, player *power4.Players) {
+	if r.Method == "POST" {
+		player.Player1 = r.FormValue("player1")
+		player.Player2 = r.FormValue("player2")
+		http.Redirect(w, r, "/difficulty", http.StatusSeeOther)
+	}
+	template, err := template.ParseFiles("./page/game.html", "./template/header.html", "./template/footer.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	template.Execute(w, player)
+}
+
+func Difficulty(w http.ResponseWriter, r *http.Request) {
+	template, err := template.ParseFiles("./page/difficulty.html", "./template/header.html", "./template/footer.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	template.Execute(w, nil)
+}
 func main() {
+	var player power4.Players
+	http.HandleFunc("/game", func(w http.ResponseWriter, r *http.Request) {
+		Player(w, r, &player)
+	})
 	http.HandleFunc("/", Home)
+	http.HandleFunc("/difficulty", Difficulty)
 	fs := http.FileServer(http.Dir("static/"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	http.ListenAndServe(":8080", nil)
--- src/game.go
+++ src/game.go
@@ -1,6 +1,8 @@
 package power4
 
 type Players struct {
-	Player1 string
-	Player2 string
+	Player1       string
+	Player2       string
+	Player1_Score int
+	Player2_Score int
 }
